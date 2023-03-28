c3b1890dd303aadf6c4d3edf7892da4c09a16183
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-28 00:47:28 -0400

    added function to test if the points are on border

---
+++ test-border.py
@@ -0,0 +1,41 @@
+import csv                      # for reading from file
+from complex import Complex     # for complex math
+
+# defines name of file being looked at
+FILENAME = "points-no-label"    # THIS IS THE ONLY THING SUPPORTED FOR NOW
+CHECKDEPTH = 1000
+
+# performs check - returns new complex number
+def check(depth, c):
+    # this is the one that will be squared
+    z = Complex(0, 0)   # REPRESENTS ITERATION 1
+
+    # does the iterations - if overflow, return None - test failed
+    for _ in range(depth):
+        z.square()      # squares 
+        z.add(c)        # adds constant
+
+    # return magnitude
+    return z.mag()
+
+# opens two files for data conversions
+with open(f"{FILENAME}.csv", "r") as inp:
+    with open("border-points.csv", "w") as out:
+        
+        # reader for first file
+        reader = csv.DictReader(inp)
+
+        # writer for the second file and writes header
+        writer = csv.writer(out)
+        writer.writerow(reader.fieldnames)
+
+        # iterates through every point
+        for point in reader:
+
+            # gets complex point
+            c = Complex(float(point["a"]), float(point["b"]))
+
+            # runs border check
+            if check(depth=CHECKDEPTH, c=c) > 1.5:
+                writer.writerow([point["a"], point["b"]])
+            
