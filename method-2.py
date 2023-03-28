507a4c604e54aba203f4b0618bdf39b3a5c0db53
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-28 00:14:39 -0400

    add import call for complex

--- method-2.py
+++ method-2.py
@@ -15,39 +15,9 @@ HEADER = ["a", "b"]
 FILENAMELABEL   = "points-labeled"
 FILENAMENOLABEL = "points-no-label"
 
-# represent complex numbers
-class Complex:
-
-    # defines how strcit to check for equalities
-    EQUALITYSTRICTNESS = 0.0000000001
-    HASHROUNDINGDIGITS = 10
-
-    # creates a new complex number a + bi
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-    
-    # square itself
-    def square(self):
-        self.a, self.b = self.a ** 2 - self.b ** 2, 2 * self.a * self.b
-
-    # add two numbers together
-    def add(self, other):
-        self.a += other.a
-        self.b += other.b
-
-    # check for equality
-    def __eq__(self, other):
-        return (abs(self.a - other.a) < Complex.EQUALITYSTRICTNESS 
-            and abs(self.b - other.b) < Complex.EQUALITYSTRICTNESS) 
-    
-    # hashes the complex number
-    def __hash__(self):
-        return int(str(hash(round(self.a, Complex.HASHROUNDINGDIGITS))) + "0000" + str(abs(hash(round(self.b, Complex.HASHROUNDINGDIGITS)))))
-
-
-import random   # for point generation
-import csv      # for writing to file
+import random                   # for point generation
+import csv                      # for writing to file
+from complex import Complex     # math with complex numbers
 
 # random number in bounds
 def randomBounded(min, max):
